d3e5e8024e7a52d084d2940fff690ffe2465b554
str818 <957133710@qq.com>
2020-04-04 10:45:57 +0800

    冒泡排序

--- src/com/str818/sort/BubbleSort.java
+++ src/com/str818/sort/BubbleSort.java
@@ -3,7 +3,7 @@ package com.str818.sort;
 /**
  * 冒泡排序
  *
- * 将最 小/大 的数交换到待排序序列的最前面。
+ * 每轮冒泡（循环）把最小/大的数放在最后。
  *
  * https://str818.github.io/2019/03/25/algorithm-sorting.html#%E4%BA%8C%E5%86%92%E6%B3%A1%E6%8E%92%E5%BA%8F
  *
