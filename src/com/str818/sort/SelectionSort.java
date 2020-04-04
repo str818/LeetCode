af1e4744cd486c6e9ec9c747a67185af481710df
str818 <957133710@qq.com>
2020-04-04 09:14:36 +0800

    选择排序

--- src/com/str818/sort/SelectionSort.java
+++ src/com/str818/sort/SelectionSort.java
@@ -13,7 +13,7 @@ package com.str818.sort;
 public class SelectionSort {
 
     public static void sort(int[] arr) {
-        if (arr == null) {
+        if (arr == null || arr.length < 2) {
             return;
         }
         for (int i = 0; i < arr.length - 1; i++) {
