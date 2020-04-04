dc713ff25fe73ea7762f80c39aa07d8bde45432a
str818 <957133710@qq.com>
2020-04-04 08:48:41 +0800

    希尔排序

--- src/com/str818/sort/ShellSort.java
+++ src/com/str818/sort/ShellSort.java
@@ -3,6 +3,10 @@ package com.str818.sort;
 /**
  * 希尔排序
  *
+ * 通过粗粒度的插入排序改善数列的有序程序，逐步细化，直到间隔为 1。
+ *
+ * https://str818.github.io/2019/03/25/algorithm-sorting.html#%E5%9B%9B%E5%B8%8C%E5%B0%94%E6%8E%92%E5%BA%8F
+ *
  * @author str818
  * @date 2020/4/4
  */
