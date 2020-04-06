1201582ae5b8fb927ad581e9f4a14b63c9a9632c
str818 <957133710@qq.com>
2020-04-06 14:39:31 +0800

    8_二叉树的下一个结点

---
+++ src/com/str818/offer/Solution_8.java
@@ -0,0 +1,45 @@
+package com.str818.offer;
+
+/**
+ * 二叉树的下一个结点
+ *
+ * 标签：树
+ *
+ * Code It Now：https://www.nowcoder.com/practice/9023a0c988684a53960365b889ceaf5e?tpId=13&tqId=11210&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+ *
+ * Note：https://str818.github.io/2019/07/08/sword-refers-to-offer-up.html#8-%E4%BA%8C%E5%8F%89%E6%A0%91%E7%9A%84%E4%B8%8B%E4%B8%80%E4%B8%AA%E7%BB%93%E7%82%B9
+ *
+ * @author str818
+ * @date 2020/4/6
+ */
+public class Solution_8 {
+
+    public TreeLinkNode GetNext(TreeLinkNode pNode) {
+        if (pNode.right != null) {
+            TreeLinkNode node = pNode.right;
+            while (node.left != null) {
+                node = node.left;
+            }
+            return node;
+        } else {
+            while (pNode.next != null) {
+                TreeLinkNode parent = pNode.next;
+                if (parent.left == pNode)
+                    return parent;
+                pNode = pNode.next;
+            }
+        }
+        return null;
+    }
+}
+
+class TreeLinkNode {
+    int val;
+    TreeLinkNode left = null;
+    TreeLinkNode right = null;
+    TreeLinkNode next = null;
+
+    TreeLinkNode(int val) {
+        this.val = val;
+    }
+}
