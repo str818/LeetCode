2a1dc19db79d8d5a4c07b0d503c58ec5d1f971ad
str818 <957133710@qq.com>
2020-04-14 09:55:37 +0800

    36_二叉搜索树与双向链表

--- src/com/str818/offer/Solution_36.java
+++ src/com/str818/offer/Solution_36.java
@@ -40,19 +40,7 @@ public class Solution_36 {
         public int val;
         public Node left;
         public Node right;
-
-        public Node() {}
-
-        public Node(int _val) {
-            val = _val;
-        }
-
-        public Node(int _val,Node _left,Node _right) {
-            val = _val;
-            left = _left;
-            right = _right;
-        }
-    };
+    }
 
 }
 
